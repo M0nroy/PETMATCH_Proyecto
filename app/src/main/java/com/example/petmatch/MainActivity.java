3af27b7f1f30af707f3af15b710e480374369786
Cristian Monroy <monroytupai@gmail.com>
2023-10-21 13:32:29 -0600

    Initial commit

---
+++ app/src/main/java/com/example/petmatch/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.petmatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler manejador = new Handler();
+
+        manejador.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                Intent objVentana = new Intent(MainActivity.this, Principal.class);
+                startActivity(objVentana);
+            }
+        }, 3000);
+    }
+}
