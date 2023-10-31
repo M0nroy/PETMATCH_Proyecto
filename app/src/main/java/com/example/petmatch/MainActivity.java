341c1199fa62da9cb96ca2a250bef2df25edc41b
Cristian Monroy <monroytupai@gmail.com>
2023-10-30 22:34:29 -0600

    se modificaron los inputs

--- app/src/main/java/com/example/petmatch/MainActivity.java
+++ app/src/main/java/com/example/petmatch/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 finish();
-                Intent objVentana = new Intent(MainActivity.this, Chat.class);
+                Intent objVentana = new Intent(MainActivity.this, inicio.class);
                 startActivity(objVentana);
             }
         }, 3000);
