af2f70206352e02830a423b28a193a1fee21ec11
Cristian Monroy <monroytupai@gmail.com>
2023-11-09 19:28:00 -0600

    se crearon los onclick de interes

--- app/src/main/java/com/example/petmatch/Interes.java
+++ app/src/main/java/com/example/petmatch/Interes.java
@@ -3,6 +3,7 @@ package com.example.petmatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Interes extends AppCompatActivity {
 
@@ -10,5 +11,19 @@ public class Interes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interes);
+    }
+
+
+
+    public void interesHombre(View view) {
+    }
+
+    public void interesMujer(View view) {
+    }
+
+    public void interesAmbos(View view) {
+    }
+
+    public void InteresSigueinte(View view) {
     }
 }
