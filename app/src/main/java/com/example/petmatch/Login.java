5dced7e7e01fef87c8edce2925d40ea5d571ffb3
Cristian Monroy <monroytupai@gmail.com>
2023-11-15 01:13:32 -0600

    ya se pueden subir las imagenes de los mejores momentos x2

--- app/src/main/java/com/example/petmatch/Login.java
+++ app/src/main/java/com/example/petmatch/Login.java
@@ -14,7 +14,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.AuthResult;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class Login extends AppCompatActivity {
 
