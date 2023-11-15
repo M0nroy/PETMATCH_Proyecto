9ea55cccfc5cd27b04b1f73ec3a7f5f62a2ed782
Cristian Monroy <monroytupai@gmail.com>
2023-11-14 21:33:06 -0600

    se implemento el registrarse y iniciar sesion

--- app/src/main/java/com/example/petmatch/Login.java
+++ app/src/main/java/com/example/petmatch/Login.java
@@ -1,14 +1,61 @@
 package com.example.petmatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class Login extends AppCompatActivity {
+
+    private EditText correo, password;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        correo = findViewById(R.id.etCorreo);
+        password = findViewById(R.id.etPassword);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    public void iniciarSesion(View view){
+
+        mAuth.signInWithEmailAndPassword(correo.getText().toString().trim(), password.getText().toString().trim())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(getApplicationContext(), "Inicio correctamente",Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(getApplicationContext(), Proposito.class);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Autenticacion Fallida",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/example/petmatch/registrarse.java
+++ app/src/main/java/com/example/petmatch/registrarse.java
@@ -2,68 +2,84 @@ package com.example.petmatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import androidx.annotation.NonNull;
 
-import com.example.petmatch.db.ControladorDB;
+
 
 public class registrarse extends AppCompatActivity {
 
-    private EditText etNombre, etApellido, etCorreo, etPassword;
-    private String nombre,apellido,correo,password;
-    ControladorDB admin;
+    private EditText nombre, apellido, correo, password;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
 
-        etNombre = findViewById(R.id.etNombre);
-        etApellido = findViewById(R.id.etApellido);
-        etCorreo = findViewById(R.id.etCorreo);
-        etPassword = findViewById(R.id.etPassword);
+        nombre = findViewById(R.id.etNombre);
+        apellido = findViewById(R.id.etApellido);
+        correo = findViewById(R.id.etCorreo);
+        password = findViewById(R.id.etPassword);
 
-        admin = new ControladorDB(this,"DBPetmatch",null,1);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 
     public void limpiarCampos(){
-        etNombre.setText("");
-        etApellido.setText("");
-        etCorreo.setText("");
-        etPassword.setText("");
+        nombre.setText("");
+        apellido.setText("");
+        correo.setText("");
+        password.setText("");
     }
 
-    public void registrarse(View view) {
-        SQLiteDatabase db = admin.getWritableDatabase();
+    public void registrarUsuario(View view){
 
-        nombre=etNombre.getText().toString();
-        apellido=etApellido.getText().toString();
-        correo=etCorreo.getText().toString();
-        password=etPassword.getText().toString();
+        mAuth.createUserWithEmailAndPassword(correo.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Registrado Correctamente", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
 
-        ContentValues registro = new ContentValues();
-        registro.put("nombre",nombre);
-        registro.put("apellido",apellido);
-        registro.put("correo",correo);
-        registro.put("password",password);
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(nombre.getText().toString() + " " + apellido.getText().toString())
+                                    .build();
+                            user.updateProfile(profileUpdates)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
 
-        long resultado = db.insert("usuarios",null,registro);
+                                                Intent i = new Intent(getApplicationContext(), PerfilUsuario.class);
+                                                startActivity(i);
+                                                limpiarCampos();
+                                            }
+                                        }
+                                    });
+                        } else {
 
-        if (resultado != -1) {
-            Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
-            limpiarCampos();
-            db.close();
-
-            Intent proposito = new Intent(this, Proposito.class);
-            startActivity(proposito);
-        } else {
-            Toast.makeText(this, "Error al registrarse", Toast.LENGTH_SHORT).show();
-        }
+                            Toast.makeText(getApplicationContext(), "Autenticacion fallida", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
-
 }
